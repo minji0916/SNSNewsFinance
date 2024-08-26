beb5de37a4cd92c566ab2ef8683dd02d2f0cd068
minji0916 <a2927036@naver.com>
2024-08-26 15:42:13 +0900

    refactor : 뉴스 스크랩해서 csv에 저장하는 코드 삭제

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 from fetch_news import fetch_all_news
-from scrape_content import scrape_content_from_csv
 import asyncio
 from async_scrape_newspaper3k import main as scrape_main
 
@@ -10,7 +9,6 @@ async def main():
     fetch_all_news()  # 뉴스 데이터 가져오기
 
     # 뉴스 url에서 본문만 스크랩
-    # scrape_content_from_csv() 
     await scrape_main()
 
 if __name__ == "__main__":

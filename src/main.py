59da0e46e81cc7a2fde59c4e055b1a3a733bd4cd
minji0916 <a2927036@naver.com>
2024-08-28 15:18:50 +0900

    딱히 변경된 것 없음

--- src/main.py
+++ src/main.py
@@ -8,12 +8,8 @@ async def main():
     전체 파이프라인을 실행합니다.
     """
     fetch_all_news()  # 뉴스 데이터 가져오기
-
-    # 뉴스 url에서 본문만 스크랩
-    await scrape_main()
-
-    # gemma2 사용해 뉴스 분석
-    summarize_news()
+    await scrape_main() # 뉴스 url에서 본문만 스크랩
+    summarize_news() # gemma2 사용해 뉴스 분석
 
 if __name__ == "__main__":
     asyncio.run(main())
